81bc8b700d9571f80c36811d61ec3004d39de268
Dewansh <dewanshrawat15@gmail.com>
2018-10-26 19:46:02 +0530

    Initial Commit
    Starting all over again

--- cyquest/cyquest/urls.py
+++ cyquest/cyquest/urls.py
@@ -1,7 +1,7 @@
 """cyquest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('quest.urls'))
 ]
