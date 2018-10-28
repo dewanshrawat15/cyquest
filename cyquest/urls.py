fc2334fa25f24683feddfdf9e261cdaa7674afda
Dewansh <dewanshrawat15@gmail.com>
2018-10-28 10:42:18 +0530

    Added logout view and navbar support for player

--- cyquest/urls.py
+++ cyquest/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', quest_view.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='quest/login.html'), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(template_name='quest/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='quest/logout.html'), name='logout'),
     path('welcome/', quest_view.welcome, name='welcome'),
     path('profile/', quest_view.profile, name='profile'),
     path('question/', quest_view.question, name='question'),
